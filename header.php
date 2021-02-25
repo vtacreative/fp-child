a6f34a256a0f689b9e151a3d950cb4db1500048c
vtacreative <funnotfame@gmail.com>
2021-02-25 15:17:30 -0800

    remove wp_title from header.php since wp_head calls it

--- header.php
+++ header.php
@@ -10,8 +10,6 @@
 	
 	<meta charset="utf-8">
 	
-	<?php wp_title( '|', true, 'right' ); ?>
-	
 	<meta name="description" content="Your company description">
 	
 	<meta name="viewport" content="width=device-width">
